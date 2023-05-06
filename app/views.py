91210936272ff52e0a50b32611efa0df70911e9f
visoraha <vinayhampi6@gmail.com>
2023-05-06 20:57:46 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from app.forms import *
 from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    
+
+    return render(request,'home.html')
+
+
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
@@ -24,9 +40,39 @@ def registration(request):
                       "hai how are you em chestunav call chey",
                       'vinayhampi31@gmail.com',
                       [user_non_save.email],
-                      fail_silently=True
+                      fail_silently=False
 
                       )
 
-        return HttpResponse('mail send successfully')    
+            return HttpResponse('registration sucessfully')
+        else:
+            return HttpResponse('in')
+   
+      
     return render(request,'registration.html',d)
+
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('password and user name wrong') 
+        
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+    
+
+
+
